8e61b343a334fa00c035a0aa570c82fcfef88020
anoopraju31 <anoopraju31@gmail.com>
2023-03-10 13:21:37 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Footer from './components/Footer'
 import Navbar from './components/Navbar'
 
 const App = () => {
 	return (
 		<div class=' dark:bg-gray-800'>
 			<Navbar />
+			<Footer />
 		</div>
 	)
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<footer className='p-4 bg-white shadow md:px-6 md:py-8 dark:bg-gray-900'>
+			<span className='block text-sm text-gray-500 text-center italic dark:text-gray-400'>
+				© Anoop Raju
+				<br />
+				Made With ❤️
+			</span>
+		</footer>
+	)
+}
+
+export default Footer
